558007c29bff22d1c18fc9d0b63a2c4888758f9c
Bsheepcoder <18947336207@163.com>
2023-09-18 05:27:19 +0800

    骨架搭建修改，解决pom依赖失效的问题

---
+++ qiyu-live-user-provider/src/main/java/org/qiyu/live/user/provider/UserProviderApplication.java
@@ -0,0 +1,19 @@
+package org.qiyu.live.user.provider;
+
+
+import org.apache.dubbo.config.spring.context.annotation.EnableDubbo;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.WebApplicationType;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+@SpringBootApplication
+@EnableDubbo
+@EnableDiscoveryClient
+public class UserProviderApplication {
+    public static void main(String[] args) {
+        SpringApplication springBootApplication = new SpringApplication(UserProviderApplication.class);
+        springBootApplication.setWebApplicationType(WebApplicationType.NONE);
+        springBootApplication.run(args);
+    }
+}
